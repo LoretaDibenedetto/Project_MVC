24c2be6e654f82c9ae55f2f2b8cd354c43fa1945
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-20 11:22:28 +0200

    add action method to create a new category add validation in create form

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,10 +9,12 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
 
         [DisplayName("Display Name")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }   
 
 
