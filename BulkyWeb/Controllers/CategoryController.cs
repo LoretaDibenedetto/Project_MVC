c783f9ad16143b8e1282e0a05ff0b5c6cfd66525
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-20 13:02:58 +0200

    add delete action

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -41,9 +41,78 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successifully";
                 return RedirectToAction("Index");   
             }
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Edit( int? id)
+        {
+
+            if(id == null || id == 0 ) 
+            {
+                return NotFound();
+            }
+            Category? categoryForDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryForDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryForDb);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edit successifully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryForDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryForDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryForDb);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category delete successifully";
+            return RedirectToAction("index");
+          
+        }
+
+
     }
+
+
+
+
+
 }
