b2a1a9ab4dc6db7c4abd38323815772c5dd84236
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-26 12:56:09 +0200

    identity fix

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,7 +8,7 @@ namespace Bulky.Utility
 {
     public static class SD
     {
-        public const string Role_Customer = "Customer";
+        public const string Role_Costumer = "Customer";
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
