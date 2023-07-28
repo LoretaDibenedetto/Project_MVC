ab2fa3ab2ee162574f23566e980717c7dabd1060
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-28 14:27:57 +0200

    add ApplicationUserRepository and Add ShoppingCartRepository

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace BulkyWeb.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> shoppingCarts{ get; set; }
+
         public DbSet<ApplicationUser> applicationUsers { get; set; }
 
 
