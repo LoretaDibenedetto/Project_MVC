79c47a2443074326b6d8dc5858a7eaf4d75fc40a
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-07-31 19:02:18 +0200

    add orderDetail and OrderHeader  repository

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -14,6 +14,9 @@ namespace Bulky.DataAccess.Repository.IRepository
         ICompanyRepository CompanyRepository { get; }
         IShoppingCartRepository ShoppingCartRepository { get; }
         IApplicationUserRepository ApplicationUserRepository { get; }
+
+        IOrderHeaderRepository OrderHeaderRepository { get; }
+        IOrderDetailRepository OrderDetailRepository { get; }
         void Save();
 
     }
